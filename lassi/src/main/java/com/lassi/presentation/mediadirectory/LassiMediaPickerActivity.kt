d09f566f1769d4d3654b18e5439f5f965b49ad8f
Hanif Shaikh <hanif.shaikh@mindinventory.com>
2024-10-21 15:20:11 +0530

    - Implemented the multi-cropping functionality.

--- lassi/src/main/java/com/lassi/presentation/mediadirectory/LassiMediaPickerActivity.kt
+++ lassi/src/main/java/com/lassi/presentation/mediadirectory/LassiMediaPickerActivity.kt
@@ -3,6 +3,7 @@ package com.lassi.presentation.mediadirectory
 import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -34,35 +35,55 @@ import com.lassi.domain.media.MediaType
 import com.lassi.domain.media.MultiLangConfig
 import com.lassi.presentation.camera.CameraFragment
 import com.lassi.presentation.common.LassiBaseViewModelActivity
+import com.lassi.presentation.cropper.CropImageContract
+import com.lassi.presentation.cropper.CropImageContractOptions
+import com.lassi.presentation.cropper.CropImageOptions
+import com.lassi.presentation.cropper.CropImageView
 import com.lassi.presentation.docs.DocsFragment
 import com.lassi.presentation.media.SelectedMediaViewModel
 import com.livefront.bridge.Bridge
 import com.livefront.bridge.SavedStateHandler
-import io.reactivex.annotations.NonNull
-import io.reactivex.annotations.Nullable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.File
 
 class LassiMediaPickerActivity :
     LassiBaseViewModelActivity<SelectedMediaViewModel, ActivityMediaPickerBinding>() {
     private var menuDone: MenuItem? = null
     private var menuCamera: MenuItem? = null
     private var menuSort: MenuItem? = null
+    private var croppedMediaList: ArrayList<MiMedia> = ArrayList()
+    private var count = 0
+    private val config = LassiConfig.getConfig()
+
+    private val cropImage = registerForActivityResult(CropImageContract()) { miMedia ->
+        miMedia?.let {
+            croppedMediaList.add(miMedia)
+            if (++count == config.selectedMedias.size) {
+                setResultOk(croppedMediaList)
+            } else {
+                val uri = Uri.fromFile(config.selectedMedias[count].path?.let { File(it) })
+                uri?.let {
+                    croppingOptions(uri = uri)
+                }
+            }
+        }
+    }
 
     private val getContent =
         registerForActivityResult(ActivityResultContracts.OpenMultipleDocuments()) { uri ->
             uri?.let { uris ->
-                if (LassiConfig.getConfig().mediaType == MediaType.FILE_TYPE_WITH_SYSTEM_VIEW && uris.size > LassiConfig.getConfig().maxCount) {
+                if (config.mediaType == MediaType.FILE_TYPE_WITH_SYSTEM_VIEW && uris.size > config.maxCount) {
                     ToastUtils.showToast(
                         this@LassiMediaPickerActivity,
-                        LassiConfig.getConfig().customLimitExceedingErrorMessage
+                        config.customLimitExceedingErrorMessage
                     )
                     finish()
                 } else {
                     binding.progressBar.indeterminateDrawable.colorFilter =
                         BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
-                            LassiConfig.getConfig().progressBarColor, BlendModeCompat.SRC_ATOP
+                            config.progressBarColor, BlendModeCompat.SRC_ATOP
                         )
                     binding.progressBar.show()
 
@@ -73,7 +94,8 @@ class LassiMediaPickerActivity :
                                     name = getFileName(uri)
                                     doesUri = false
                                     fileSize = getFileSize(uri)
-                                    path = getFilePathFromUri(this@LassiMediaPickerActivity, uri, true)
+                                    path =
+                                        getFilePathFromUri(this@LassiMediaPickerActivity, uri, true)
                                     Log.d("TAG", "!@# SLOWER MEDIA ITEM: $name")
                                 }
                             } as ArrayList<MiMedia>
@@ -106,13 +128,13 @@ class LassiMediaPickerActivity :
     override fun initViews() {
         super.initViews()
         Bridge.initialize(applicationContext, object : SavedStateHandler {
-            override fun saveInstanceState(@NonNull target: Any, @NonNull state: Bundle) {
+            override fun saveInstanceState(target: Any, state: Bundle) {
             }
 
-            override fun restoreInstanceState(@NonNull target: Any, @Nullable state: Bundle?) {
+            override fun restoreInstanceState(target: Any, state: Bundle?) {
             }
         })
-        setToolbarTitle(LassiConfig.getConfig().selectedMedias)
+        setToolbarTitle(config.selectedMedias)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setThemeAttributes()
@@ -120,12 +142,12 @@ class LassiMediaPickerActivity :
     }
 
     private fun setToolbarTitle(selectedMedias: ArrayList<MiMedia>) {
-        val maxCount = LassiConfig.getConfig().maxCount
+        val maxCount = config.maxCount
         if (maxCount > 1) {
             binding.toolbar.title = String.format(
                 getString(R.string.selected_items),
-                selectedMedias.size,
-                maxCount
+                selectedMedias.size.toString(),
+                maxCount.toString()
             )
         } else {
             binding.toolbar.title = ""
@@ -133,7 +155,7 @@ class LassiMediaPickerActivity :
     }
 
     private fun initiateFragment() {
-        if (LassiConfig.getConfig().lassiOption == LassiOption.CAMERA) {
+        if (config.lassiOption == LassiOption.CAMERA) {
             supportFragmentManager.beginTransaction()
                 .replace(
                     R.id.ftContainer,
@@ -141,7 +163,7 @@ class LassiMediaPickerActivity :
                 )
                 .commitAllowingStateLoss()
         } else {
-            LassiConfig.getConfig().mediaType.let { mediaType ->
+            config.mediaType.let { mediaType ->
                 when (mediaType) {
                     MediaType.DOC -> {
                         supportFragmentManager.beginTransaction()
@@ -171,7 +193,7 @@ class LassiMediaPickerActivity :
 
     private fun browseFile() {
         val mimeTypesList = ArrayList<String>()
-        LassiConfig.getConfig().supportedFileType.forEach { mimeType ->
+        config.supportedFileType.forEach { mimeType ->
             MimeTypeMap
                 .getSingleton()
                 .getMimeTypeFromExtension(mimeType)?.let {
@@ -184,7 +206,7 @@ class LassiMediaPickerActivity :
     }
 
     private fun setThemeAttributes() {
-        with(LassiConfig.getConfig()) {
+        with(config) {
             binding.toolbar.background =
                 ColorDrawable(toolbarColor)
             binding.toolbar.setTitleTextColor(toolbarResourceColor)
@@ -210,29 +232,27 @@ class LassiMediaPickerActivity :
         menuDone?.isVisible = false
         menuCamera?.isVisible = false
         menuSort?.isVisible = true
-
-        menuDone?.icon = changeIconColor(
-            this@LassiMediaPickerActivity,
-            R.drawable.ic_done_white,
-            LassiConfig.getConfig().toolbarResourceColor
-        )
-        menuCamera?.icon = changeIconColor(
-            this@LassiMediaPickerActivity,
-            R.drawable.ic_camera_white,
-            LassiConfig.getConfig().toolbarResourceColor
-        )
-        menuSort?.icon = changeIconColor(
-            this@LassiMediaPickerActivity,
-            R.drawable.ic_sorting_foreground,
-            LassiConfig.getConfig().toolbarResourceColor
-        )
-        return super.onCreateOptionsMenu(menu)
+        with(config) {
+            menuDone?.icon = changeIconColor(
+                this@LassiMediaPickerActivity,
+                R.drawable.ic_done_white, toolbarResourceColor
+            )
+            menuCamera?.icon = changeIconColor(
+                this@LassiMediaPickerActivity,
+                R.drawable.ic_camera_white, toolbarResourceColor
+            )
+            menuSort?.icon = changeIconColor(
+                this@LassiMediaPickerActivity,
+                R.drawable.ic_sorting_foreground, toolbarResourceColor
+            )
+            return super.onCreateOptionsMenu(menu)
+        }
     }
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
         menuCamera?.isVisible =
-            (LassiConfig.getConfig().lassiOption == LassiOption.CAMERA ||
-                    LassiConfig.getConfig().lassiOption == LassiOption.CAMERA_AND_GALLERY)
+            (config.lassiOption == LassiOption.CAMERA ||
+                    config.lassiOption == LassiOption.CAMERA_AND_GALLERY)
         menuDone?.isVisible = !viewModel.selectedMediaLiveData.value.isNullOrEmpty()
         return super.onPrepareOptionsMenu(menu)
     }
@@ -248,18 +268,27 @@ class LassiMediaPickerActivity :
 
     private fun setSelectedMediaResult() {
         // Allow crop for single image
-        when (LassiConfig.getConfig().mediaType) {
-            MediaType.IMAGE, MediaType.VIDEO, MediaType.AUDIO, MediaType.DOC -> {
-                setResultOk(viewModel.selectedMediaLiveData.value)
-            }
+        config.apply {
+            when (mediaType) {
+                MediaType.IMAGE, MediaType.VIDEO, MediaType.AUDIO, MediaType.DOC -> {
+                    if (mediaType == MediaType.IMAGE && isCrop) {
+                        val uri = Uri.fromFile(config.selectedMedias.first().path?.let { File(it) })
+                        uri?.let {
+                            croppingOptions(uri = uri)
+                        }
+                    } else {
+                        setResultOk(viewModel.selectedMediaLiveData.value)
+                    }
+                }
 
-            else -> {
+                else -> {
+                }
             }
         }
     }
 
     private fun initCamera() {
-        if (viewModel.selectedMediaLiveData.value?.size == LassiConfig.getConfig().maxCount) {
+        if (viewModel.selectedMediaLiveData.value?.size == config.maxCount) {
             ToastUtils.showToast(this, MultiLangConfig.getConfig().alreadySelectedMaxItems)
         } else {
             supportFragmentManager.beginTransaction()
@@ -271,7 +300,7 @@ class LassiMediaPickerActivity :
 
     private fun handleSelectedMedia(selectedMedias: ArrayList<MiMedia>) {
         setToolbarTitle(selectedMedias)
-        menuDone?.isVisible = !selectedMedias.isNullOrEmpty()
+        menuDone?.isVisible = selectedMedias.isNotEmpty()
     }
 
     private fun setResultOk(selectedMedia: ArrayList<MiMedia>?) {
@@ -284,5 +313,35 @@ class LassiMediaPickerActivity :
         )
         setResult(Activity.RESULT_OK, intent)
         finish()
+    }
+
+    private fun croppingOptions(
+        uri: Uri? = null, includeCamera: Boolean? = false, includeGallery: Boolean? = false
+    ) {
+        // Start picker to get image for cropping and then use the image in cropping activity.
+        cropImage.launch(includeCamera?.let {
+            includeGallery?.let { includeGallery ->
+                config.cropAspectRatio?.x?.let { x ->
+                    config.cropAspectRatio?.y?.let { y ->
+                        CropImageOptions(
+                            imageSourceIncludeCamera = includeCamera,
+                            imageSourceIncludeGallery = includeGallery,
+                            cropShape = config.cropType,
+                            showCropOverlay = true,
+                            guidelines = CropImageView.Guidelines.ON,
+                            multiTouchEnabled = false,
+                            aspectRatioX = x,
+                            aspectRatioY = y,
+                            fixAspectRatio = config.enableActualCircleCrop,
+                        )
+                    }
+                }
+            }
+        }?.let {
+            CropImageContractOptions(
+                uri = uri,
+                cropImageOptions = it,
+            )
+        })
     }
 }
